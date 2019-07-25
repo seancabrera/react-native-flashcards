c824f2ec7b30be7eae8916aa4ed94d9d8d968075
Sean Cabrera <sean.cabrera@gmail.com>
2019-07-24 20:25:55 -0600

    Implement the UI for adding decks

--- src/components/AddDeckScreen.js
+++ src/components/AddDeckScreen.js
@@ -1,13 +1,43 @@
 import React from 'react';
 import { StyleSheet, View, Text } from 'react-native';
-import { ListItem } from 'react-native-elements';
-import { Platform, StatusBar} from 'react-native';
+import { Input, Button } from 'react-native-elements';
+import { Platform, StatusBar } from 'react-native';
 
 export default class AddDeckScreen extends React.Component {
+  constructor(props) {
+    super(props);
+    this.setState({
+      newDeckTitle: ''
+    });
+
+    this.onInputChanged = this.onInputChanged.bind(this);
+    this.submitNewDeck = this.submitNewDeck.bind(this);
+  }
+
+  onInputChanged(title) {
+    this.setState({
+      newDeckTitle: title
+    });
+  }
+
+  submitNewDeck() {
+    // TODO submit to AsyncStorage
+
+    this.props.navigation.navigate('Decks');
+  }
+
   render() {
     return (
       <View style={styles.container}>
-        <Text>Test Add Screen!</Text>
+        <Text>What is the title of your new deck?</Text>
+        <Input
+          onChangeText={this.onInputChanged}
+        />
+        <Button
+          buttonStyle={styles.submitButton}
+          title="Submit"
+          onPress={this.submitNewDeck}
+        />
       </View>
     );
   }
@@ -20,5 +50,8 @@ const styles = StyleSheet.create({
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center'
+  },
+  submitButton: {
+    marginTop: 50
   }
 });
